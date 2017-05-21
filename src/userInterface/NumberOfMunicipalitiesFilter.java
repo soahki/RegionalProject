fc3621e9a351cdc7c2103f0e1f95ec8ff29c051a
Anders Björkland <a.bjorkland@live.se>
2017-05-21 09:08:47 +0200

    Added Runnable objects to render the images in class NumberOfMunicipalitiesFilter. (Halving the loading time.)

--- src/userInterface/NumberOfMunicipalitiesFilter.java
+++ src/userInterface/NumberOfMunicipalitiesFilter.java
@@ -9,8 +9,7 @@ import localities.Municipality;
 import localities.Region;
 
 import java.io.File;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class NumberOfMunicipalitiesFilter implements Filter {
     private Map<Region, List<Municipality>> regionMap;
@@ -19,22 +18,53 @@ public class NumberOfMunicipalitiesFilter implements Filter {
         this.regionMap = regionMap;
     }
 
-
     @Override
     public StackPane getFilteredImages() {
         StackPane stackPane = new StackPane();
+        //Set<ImageView> images = new HashSet<>();
+        ImageView[] images = new ImageView[regionMap.keySet().size()];
+        Thread[] threads = new Thread[regionMap.keySet().size()];
+        int i = 0;
         for (Region region : regionMap.keySet()) {
-            File file = new File("./resources/images/regions/" + region.getRegionCode() + ".png");
-            ImageView image = OverviewMap.getColoredImage(
-                    file, getColorFromRegions(region), new Dimension2D(300, 500));
-            stackPane.getChildren().add(image);
+            int index = i;
+            Runnable renderer = () -> {
+                File file = new File("./resources/images/regions/" + region.getRegionCode() + ".png");
+                ImageView image = OverviewMap.getColoredImage(
+                        file, getColorFromRegions(region), new Dimension2D(300, 500));
+                //images.add(image);
+                images[index] = image;
+            };
+            threads[i] = new Thread(renderer);
+            i++;
         }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        while (true) {
+            int numRunning = 0;
+            for (Thread thread : threads) {
+                if (thread.isAlive()) {
+                    numRunning++;
+                }
+            }
+
+            if (numRunning == 0) {
+                break;
+            }
+
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        stackPane.getChildren().addAll(images);
         return stackPane;
     }
 
     public Color getColorFromRegions(Region region) {
-        int red = 0;
-        red = 50 + 4 * (regionMap.get(region).size());
+        int red = 50 + 4 * (regionMap.get(region).size());
         if (red > 255)
             red = 255;
         return Color.rgb(red, 0, 0);
